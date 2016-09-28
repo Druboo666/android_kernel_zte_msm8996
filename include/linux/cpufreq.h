689e50c1cfc36bee6e21470f612b4cb7ee695f89
Eliminater74 <eliminater74@gmail.com>
2016-09-28 09:32:50 -0400

    cpufreq: Gov: Introduce BioShock
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>
    
    # Conflicts:
    #	drivers/cpufreq/Kconfig
    #	drivers/cpufreq/Makefile
    #	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -526,6 +526,9 @@ extern struct cpufreq_governor cpufreq_gov_yankactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
 extern struct cpufreq_governor cpufreq_gov_ironactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
 #endif
 
 /*********************************************************************
